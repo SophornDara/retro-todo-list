a2cce3ebe33a890a7336c4d2e774f3dfab763060
Sophorndara <d4ra04@gmail.com>
2026-06-02 10:22:46 +0700

    footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "./Text";
+
+export function Footer() {
+  return (
+    <footer className="mt-auto py-8 text-center border-t-4 border-black">
+      <Text variant="p" className="uppercase font-bold">
+        @sophorndara
+      </Text>
+    </footer>
+  );
+}
